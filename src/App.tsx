f385538c1ffd8f07983b538c43e129e0a89fe9fc
isid555 <r555sid@gmail.com>
2025-04-22 09:52:06 +0530

    favicon commit

--- src/App.tsx
+++ src/App.tsx
@@ -12,16 +12,20 @@ const App = () => (
   <QueryClientProvider client={queryClient}>
     <TooltipProvider>
       <Toaster />
-      <Sonner />
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<Index />} />
-          {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
           <Route path="*" element={<NotFound />} />
         </Routes>
       </BrowserRouter>
     </TooltipProvider>
   </QueryClientProvider>
+
+
+
+
+
+
 );
 
 export default App;
